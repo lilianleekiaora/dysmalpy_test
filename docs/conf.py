332e253bc7a51b5ee87ebcdbaaf346069e2399aa
Lilian Lee <22501656+lilianleekiaora@users.noreply.github.com>
2024-09-21 13:28:30 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,15 @@ from distutils.version import LooseVersion
 import dysmalpy
 
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # -- Project information -----------------------------------------------------
 
 project = 'dysmalpy'
